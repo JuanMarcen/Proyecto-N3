8c0f209b4dc48c8638db19451d079e9aaa6dc8ef
JuanMarcen <jumarguti2002@gmail.com>
2025-09-10 11:56:49 +0200

    commit from Rstudio

--- 01_dataframes.R
+++ 01_dataframes.R
@@ -102,3 +102,24 @@ df_days <- df_hours %>%
 
 save(estaciones, df_minutes, df_hours, df_days,
      file = 'data.RData')
+
+
+par(mfrow = c(4,4))
+aux.ind <- df_hours$t + (df_hours$mes - 0.5)/12
+
+aux.x <- tapply(aux.ind, aux.ind, mean )
+for(i in estaciones){
+  aux.y <- tapply(!is.na(df_hours[[paste0(i, '.p')]]), aux.ind, sum)
+  plot(aux.x, aux.y, main = i)
+  
+}
+
+
+basura1 <- apply(df_hours[, paste0(estaciones, '.p')] > 0, 1, mean, na.rm = T)
+hist(basura)
+basura2 <- apply(df_hours[, paste0(estaciones, '.p')], 1, mean, na.rm = T)
+summary(basura[basura > 0])
+hist(basura[basura > 0])
+basura3 <- apply(df_hours[, paste0(estaciones, '.p')], 1, max, na.rm = T)
+summary(basura3[basura3 > 0])
+hist(basura3[basura3 > 0])
