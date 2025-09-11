51611afef45843ecdf67cd8eb822164daf80d67d
JuanMarcen <jumarguti2002@gmail.com>
2025-09-11 10:44:45 +0200

    commit from Rstudio

--- Exploratorio/Exploratorio.R
+++ Exploratorio/Exploratorio.R
@@ -28,46 +28,87 @@ for(i in estaciones){
   
 }
 
+dev.off()
+
 #----Descriptive analysis of 0's and Non-0's----
 # HOURLY AND 15' (FIRST HOURLY)
 # I need info for each station
-# example (1 station)
+
 # objective. 1 dataframe for each station
-sta <- estaciones[1]
+df.desc.mes <- function(station, data){
+  
+  #info per months (due to possible stationality)
+  df <- as.data.frame(matrix(NA, ncol = 1, nrow = 12))
+  colnames(df) <- 'Mes'
+  df$Mes <- sort(unique(data$mes))
+  
+  df_station <- data[, c('t', 'l', 'mes', 'dia.mes', paste0(station, '.p'))]
+  
+  # things to compute
+  # 1. rel. freq of 0's for each month (not taking into account NA's)
+  cant0 <- tapply(df_station[[paste0(station, '.p')]] == 0, df_station$mes, sum, na.rm = T)
+  total <- tapply(!is.na(df_station[, paste0(station, '.p')]), df_station$mes, sum)
+  freq0 <- as.data.frame(cant0 / total * 100)
+  
+  df$f.rel.0 <- round(freq0[, 1], 3)
+  
+  # 2. For values greater than 0
+  df_no0 <- df_station %>%
+    filter(.data[[paste0(station, ".p")]] > 0)
+  
+  # 2.1 media
+  media <- data.frame(tapply(df_no0[, paste0(station, '.p')], df_no0$mes, mean, na.rm = T))
+  df$media <- round(media[, 1], 3)
+  
+  # 2.2 mediana
+  mediana <- data.frame(tapply(df_no0[, paste0(station, '.p')], df_no0$mes, median, na.rm = T))
+  df$mediana <- round(mediana[, 1], 3)
+  
+  # 2.3 cuantiles 0.90, 0.99 y 0.95
+  quant.geq.0 <- data.frame(tapply(df_no0[, paste0(station, '.p')], df_no0$mes, quantile, probs = c(0.9),na.rm = T))
+  df$q0.90 <- round(quant.geq.0[, 1], 3)
+  
+  quant.geq.0 <- data.frame(tapply(df_no0[, paste0(station, '.p')], df_no0$mes, quantile, probs = c(0.95),na.rm = T))
+  df$q0.95 <- round(quant.geq.0[, 1], 3)
+  
+  quant.geq.0 <- data.frame(tapply(df_no0[, paste0(station, '.p')], df_no0$mes, quantile, probs = c(0.99),na.rm = T))
+  df$q0.99 <- round(quant.geq.0[, 1], 3)
+  
+  # 2.4 máximo
+  max <- data.frame(tapply(df_no0[, paste0(station, '.p')], df_no0$mes, max, na.rm = T))
+  df$max <- round(max.geq.0[, 1], 3)
+  
+  return(df)
+}
 
-#info per months (due to possible stationality)
-del <- as.data.frame(matrix(NA, ncol = 1, nrow = 12))
-colnames(del) <- 'Mes'
-del$Mes <- sort(unique(df_hours$mes))
+for (station in estaciones){
+  assign(paste0('df.h.desc.', station), df.desc.mes(station, df_hours))
+}   
 
-df_station <- df_hours[, c('t', 'l', 'mes', 'dia.mes', paste0(sta, '.p'))]
+# in case we want the info for data every 15'
+for (station in estaciones){
+  assign(paste0('df.min.desc.', station), df.desc.mes(station, df_minutes))
+}   
 
-# things to compute
-# 1. rel. freq of 0's for each month (not taking into account NA's)
-cant0 <- tapply(df_station[[paste0(sta, '.p')]] == 0, df_station$mes, sum, na.rm = T)
-total <- tapply(!is.na(df_station[, paste0(sta, '.p')]), df_station$mes, sum)
-freq0 <- as.data.frame(cant0 / total * 100)
+# table writing for Word
+library(writexl)
+write_xlsx(df.h.desc.EM71, "borrar.xlsx")
 
-del$freq.rel.0 <- freq0[, 1]
-
-# 2. For values greater than 0
-# 2.1 Aproxximate distribution (all and per month)
-#they should be really asymmetric
+# 3 Aproxximate distribution (all and per month) for non 0's
 df_no0 <- df_station %>%
-  filter(.data[[paste0(sta, ".p")]] > 0)
+  filter(.data[[paste0(station, ".p")]] > 0)
 
-
-dens <- density(df_no0[, paste0(sta, ".p")], 
-                from = min(df_no0[, paste0(sta, ".p")]),
-                to   = max(df_no0[, paste0(sta, ".p")]))
-plot(dens, lwd = 2, main = paste("Densidades de", sta), xlab = "Valor")
+dens <- density(df_no0[, paste0(station, ".p")], 
+                from = min(df_no0[, paste0(station, ".p")]),
+                to   = max(df_no0[, paste0(station, ".p")]))
+plot(dens, lwd = 2, main = paste("Densidades de", station), xlab = "Valor")
 
 col_mes <- rainbow(12)
 for (i in 1:12){
   ind <- which(df_no0$mes == i)
-  dens <- density(df_no0[ind, paste0(sta, ".p")], 
-                  from = min(df_no0[ind, paste0(sta, ".p")]),
-                  to   = max(df_no0[ind, paste0(sta, ".p")]))
+  dens <- density(df_no0[ind, paste0(station, ".p")], 
+                  from = min(df_no0[ind, paste0(station, ".p")]),
+                  to   = max(df_no0[ind, paste0(station, ".p")]))
   lines(dens, col = col_mes[i])
   
 }
@@ -75,32 +116,10 @@ for (i in 1:12){
 legend("topright", legend = c("Total", paste("Mes", 1:12)),
        col = c("black", col_mes), lwd = 2)
 
-
-# 2.2 media
-mean.geq.0 <- data.frame(tapply(df_no0[, paste0(sta, '.p')], df_no0$mes, mean, na.rm = T))
-del$mean.geq.0 <- mean.geq.0[, 1]
-
-# 2.3 mediana
-med.geq.0 <- data.frame(tapply(df_no0[, paste0(sta, '.p')], df_no0$mes, median, na.rm = T))
-del$med.geq.0 <- med.geq.0[, 1]
-
-# 2.4 cuantiles 0.90, 0.99 y 0.95
-quant.geq.0 <- data.frame(tapply(df_no0[, paste0(sta, '.p')], df_no0$mes, quantile, probs = c(0.9),na.rm = T))
-del$q0.90.geq.0 <- quant.geq.0[, 1]
-
-quant.geq.0 <- data.frame(tapply(df_no0[, paste0(sta, '.p')], df_no0$mes, quantile, probs = c(0.95),na.rm = T))
-del$q0.95.geq.0 <- quant.geq.0[, 1]
-
-quant.geq.0 <- data.frame(tapply(df_no0[, paste0(sta, '.p')], df_no0$mes, quantile, probs = c(0.99),na.rm = T))
-del$q0.99.geq.0 <- quant.geq.0[, 1]
-
-# 2.5 máximo
-max.geq.0 <- data.frame(tapply(df_no0[, paste0(sta, '.p')], df_no0$mes, max, na.rm = T))
-del$max.geq.0 <- max.geq.0[, 1]                        
                         
                         
 
-# eventos extremos, para ver cuadno son las fechas --> dependencias espaciales
+#----eventos extremos, para ver cuadno son las fechas --> dependencias espaciales----
 basura1 <- apply(df_hours[, paste0(estaciones, '.p')] > 0, 1, mean, na.rm = T)
 hist(basura)
 basura2 <- apply(df_hours[, paste0(estaciones, '.p')], 1, mean, na.rm = T)
