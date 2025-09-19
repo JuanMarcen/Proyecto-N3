792d8861fa642251b661c91c2cc71bd8d040ce9a
JuanMarcen <jumarguti2002@gmail.com>
2025-09-19 15:34:36 +0200

    commit from Rstudio

--- 04_Dataframes_ERA5.R
+++ 04_Dataframes_ERA5.R
@@ -78,9 +78,80 @@ for(ss in 1:nrow(stations)){
 
 # Combine global data.frame with ERA5 variables
 gdf <- cbind(gdf,gmat)
-
 gdf <- gdf[gdf$date >= ref_period[1] & gdf$date <= ref_period[2], ]
+
+#now the TEMPERATURES
+# Target grid points
+# Corner points, last 4 are the ones we will use
+cpoints.4 <- c("42N.2W", "42N.1W", "41N.2W", "41N.1W")
+# Near-station points
+tpoints <- cpoints.4
+# Reference period
+# Read grid data
+era5 <- cbind(readRDS('datos_elsa/t500.rds'), readRDS('datos_elsa/t700.rds'))
+# Convert ERA5 geopotentials into geopotential height
+#era5 <- era5[-which(format(era5$Date, '%m-%d') == '02-29'), ]
+era5_vars <- grep("t",names(era5))
+era5[,era5_vars] <- era5[,era5_vars] - 273.15 #temp in ºC
+names(era5)[era5_vars] <- paste0("z",names(era5)[era5_vars])
+
+# Reshape data
+era5$Date <- as.Date(era5$Date)
+TT <- length(table(format(era5$Date,"%Y")))
+LL <- nrow(era5)/TT
+SS <- nrow(stations)
+
+# Init global data.frame
+tdf <- data.frame(STAID = rep(stations$STAID, each = nrow(era5)),
+                  date = rep(era5$Date, SS),
+                  t = rep(rep(1:TT, each = LL), SS),
+                  l = rep(rep(1:LL, TT), SS),
+                  LAT = rep(stations$LAT, each = nrow(era5)),
+                  LON = rep(stations$LON, each = nrow(era5)),
+                  grid = rep(stations$Grid, each = nrow(era5)))
+
+# Variable names
+eralevels <- paste0("tg",c("300", "500","700"))
+# Corner points
+cpoints.4 <- c("42N.2W", "42N.1W", "41N.2W", "41N.1W")
+# Define variable names
+vnames <- paste(rep(eralevels, length(cpoints.4)+1),
+                rep(c("",cpoints.4),each = length(eralevels)),
+                sep = ".")
+
+# Global variables
+gmat <- matrix(data = as.numeric(NA),
+               nrow = nrow(tdf),
+               ncol = length(vnames))
+colnames(gmat) <- vnames
+gmat <- as.data.frame(gmat)
+
+# Read closed point for each station and concatenate in the global matrix
+for(ss in 1:nrow(stations)){
+  
+  cat(paste0("..",ss))
+  
+  # Concatenating indices
+  cidx1 <- 1+nrow(era5)*(ss-1)
+  cidx2 <- nrow(era5)*ss
+  
+  # Include variables of interest
+  fivep <- c(stations$Grid[ss], cpoints.4)
+  eranames <- paste(rep(eralevels, length(fivep)),
+                    rep(fivep, each = length(eralevels)),
+                    sep = ".")
+  # Add to matrix
+  gmat[cidx1:cidx2,] <- era5[,match(eranames, names(era5))]
+  
+}# for ss stations
+
+# Combine global data.frame with ERA5 variables
+tdf <- cbind(tdf,gmat)
+
+tdf <- tdf[tdf$date >= ref_period[1] & tdf$date <= ref_period[2], ]
+
+global_df <- cbind(gdf, tdf[, ])
 # Write data.frame
-saveRDS(object = gdf,
+saveRDS(object = global_df,
         file = "global_df.rds")
 
---
+++ datos_elsa/transformacion.R
@@ -0,0 +1,163 @@
+rm(list=ls())
+
+#transformación de datos a como deseo 
+
+#geopotenciales
+lon <- readRDS("datos_elsa/lon.rds")
+lat <- readRDS("datos_elsa/lat.rds")
+g_700_12pm_60_23 <- readRDS("datos_elsa/g_700_12pm_60_23.rds")
+g_500_12pm_60_23 <- readRDS("datos_elsa/g_500_12pm_60_23.rds")
+g_300_12pm_60_23 <- readRDS("datos_elsa/g_300_12pm_60_23.rds")
+Date <- readRDS("datos_elsa/Date.rds")
+
+library(dplyr)
+library(lubridate)
+Date1<-as.Date(Date,format = "%m/%d/%Y")
+
+#quitar 29 febrero
+ind <- which(format(Date1, '%m-%d') != '02-29')
+
+Date <- Date1[ind]
+lat <- lat[ind]
+lon <- lon[ind]
+g300 <- g_300_12pm_60_23[ind]
+g500 <- g_500_12pm_60_23[ind]
+g700 <- g_700_12pm_60_23[ind]
+
+#coordenadas unicas 11x16
+coord <- data.frame(lat=lat,lon=lon)
+coord <- unique(coord)
+coord$station <- paste(coord$lat, coord$lon, sep = "_")
+
+head(coord)
+
+coord<-coord[order(-coord$lat),] #de izq a derecha
+head(coord)
+orden_estaciones <- unique(coord$station)
+head(orden_estaciones)
+
+#g300
+g300 <- data.frame(Date = Date, lat = lat, lon = lon, g300 = g300)
+g300$station <- paste0(round(g300$lat),"N.",abs(round(g300$lon)),ifelse(g300$lon>-0.5,"E","W"))
+g300$lat<-NULL
+g300$lon<-NULL
+
+# Convertir el data frame de largo a ancho
+g300 <- reshape(g300, 
+                    idvar = "Date", 
+                    timevar = "station", 
+                    direction = "wide")
+
+#colnames(g300)[-1] <- sub("^g300\\.", "", colnames(g300)[-1])
+head(g300)
+dim(g300)
+
+#g500
+g500 <- data.frame(Date = Date, lat = lat, lon = lon, g500 = g500)
+g500$station <- paste0(round(g500$lat),"N.",abs(round(g500$lon)),ifelse(g500$lon>-0.5,"E","W"))
+g500$lat<-NULL
+g500$lon<-NULL
+
+# Convertir el data frame de largo a ancho
+g500 <- reshape(g500, 
+                    idvar = "Date", 
+                    timevar = "station", 
+                    direction = "wide")
+
+
+#g700
+g700 <- data.frame(Date = Date, lat = lat, lon = lon, g700 = g700)
+g700$station <- paste0(round(g700$lat),"N.",abs(round(g700$lon)),ifelse(g700$lon>-0.5,"E","W"))
+g700$lat<-NULL
+g700$lon<-NULL
+
+# Convertir el data frame de largo a ancho
+g700 <- reshape(g700, 
+                    idvar = "Date", 
+                    timevar = "station", 
+                    direction = "wide")
+
+
+saveRDS(coord, "datos_elsa/coord_g.rds")
+saveRDS(g300, "datos_elsa/g300.rds")
+saveRDS(g500, "datos_elsa/g500.rds")
+saveRDS(g700, "datos_elsa/g700.rds")
+
+# temperaturas
+rm(list = ls())
+lon <- readRDS("datos_elsa/lon_t.rds")
+lat <- readRDS("datos_elsa/lat_t.rds")
+t_500_12pm_60_23 <- readRDS("datos_elsa/t_500_12pm_60_23.rds")
+t_700_12pm_60_23 <- readRDS("datos_elsa/t_700_12pm_60_23.rds")
+Date <- readRDS("datos_elsa/Date_t.rds")
+
+Date1<-as.Date(Date,format = "%m/%d/%Y")
+
+#quitar 29 febrero
+ind <- which(format(Date1, '%m-%d') != '02-29')
+
+Date <- Date1[ind]
+lat <- lat[ind]
+lon <- lon[ind]
+#t300 <- t_300_12pm_60_23[ind]
+t500 <- t_500_12pm_60_23[ind]
+t700 <- t_700_12pm_60_23[ind]
+
+#coordenadas unicas 11x16
+coord <- data.frame(lat=lat,lon=lon)
+coord <- unique(coord)
+coord$station <- paste(coord$lat, coord$lon, sep = "_")
+
+head(coord)
+
+coord<-coord[order(-coord$lat),] #de izq a derecha
+head(coord)
+orden_estaciones <- unique(coord$station)
+head(orden_estaciones)
+
+#t300
+# t300 <- data.frame(Date = Date, lat = lat, lon = lon, t300 = t300)
+# t300$station <- paste0(round(t300$lat),"N.",abs(round(t300$lon)),ifelse(t300$lon>-0.5,"E","W"))
+# t300$lat<-NULL
+# t300$lon<-NULL
+# 
+# # Convertir el data frame de largo a ancho
+# t300 <- reshape(t300, 
+#                 idvar = "Date", 
+#                 timevar = "station", 
+#                 direction = "wide")
+# 
+
+
+#t500
+t500 <- data.frame(Date = Date, lat = lat, lon = lon, t500 = t500)
+t500$station <- paste0(round(t500$lat),"N.",abs(round(t500$lon)),ifelse(t500$lon>-0.5,"E","W"))
+t500$lat<-NULL
+t500$lon<-NULL
+
+# Convertir el data frame de largo a ancho
+t500 <- reshape(t500, 
+                idvar = "Date", 
+                timevar = "station", 
+                direction = "wide")
+
+
+
+#t700
+t700 <- data.frame(Date = Date, lat = lat, lon = lon, t700 = t700)
+t700$station <- paste0(round(t700$lat),"N.",abs(round(t700$lon)),ifelse(t700$lon>-0.5,"E","W"))
+t700$lat<-NULL
+t700$lon<-NULL
+
+# Convertir el data frame de largo a ancho
+t700 <- reshape(t700, 
+                idvar = "Date", 
+                timevar = "station", 
+                direction = "wide")
+
+
+
+saveRDS(coord, "datos_elsa/coord_t.rds")
+#saveRDS(t300, "datos_elsa/t300.rds")
+saveRDS(t500, "datos_elsa/t500.rds")
+saveRDS(t700, "datos_elsa/t700.rds")
